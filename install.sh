1d941bf3d863e4939a75b611a37a28555d813b64
Stephen Paulger <stephen.paulger@newspeak.org.uk>
2012-12-05 10:26:55 +0000

    improve install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+mkdir -p ~/.vim/ftplugin/
+
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/tmux.conf ~/.tmux.conf
+ln -s $PWD/ackrc ~/.ackrc
 
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vim/ftplugin/python.vim ~/.vim/ftplugin/python.vim
