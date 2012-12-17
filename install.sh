8ecd3ca9f99b054ab0c529ca79c4d966f5276fce
Stephen Paulger <stephen.paulger@newspeak.org.uk>
2012-12-17 19:04:05 +0000

    Fix zshrc backup

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
-[[ -f ~/.zshrc ]] && mv ~/.zsh ~/.zsh.preconf
+[[ -f ~/.zshrc ]] && mv ~/.zshrc ~/.zsh.preconf
 
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/tmux.conf ~/.tmux.conf
