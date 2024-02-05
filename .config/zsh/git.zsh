9745f1229af8811f43f05b9e42b4acdf7cd6e9f4
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2024-02-05 13:56:16 +0000

    Get eza, homebrew working, add git aliases

---
+++ .config/zsh/git.zsh
@@ -0,0 +1,22 @@
+# Check if main exists and use instead of master
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,master}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return 0
+    fi
+  done
+
+  # If no main branch was found, fall back to master but return error
+  echo master
+  return 1
+}
+
+
+alias gc='git commit --verbose'
+alias gcm='git checkout $(git_main_branch)'
+alias gl="git pull"
+alias gss="git status --short"
+alias gp="git push"
--- .zshrc
+++ .zshrc
@@ -1,7 +1,12 @@
 # Created by Zap installer
 [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+
+plug "$HOME/.config/zsh/git.zsh"
+plug "$HOME/.config/zsh/homebrew.zsh"
+
 plug "zsh-users/zsh-autosuggestions"
 plug "zap-zsh/supercharge"
+plug "zap-zsh/exa"
 plug "zap-zsh/zap-prompt"
 plug "zsh-users/zsh-syntax-highlighting"
 
