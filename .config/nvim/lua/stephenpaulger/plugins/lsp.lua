4400d71db229ee09aa051013df3ecd5e461f2380
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2024-02-07 22:58:06 +0000

    Add completions and dashboard

--- .config/nvim/lua/stephenpaulger/plugins/lsp.lua
+++ .config/nvim/lua/stephenpaulger/plugins/lsp.lua
@@ -1,27 +1,30 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup()
         end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                },
-            })
-        end,
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
         end,
     },
--- .config/nvim/lua/stephenpaulger/plugins/treesitter.lua
+++ .config/nvim/lua/stephenpaulger/plugins/treesitter.lua
@@ -1,3 +1,14 @@
 return {
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                auto_install = true,
+                hightlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
 }
