80090bb9463cbaa372074671b6f6637ebfba3f75
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2024-02-09 13:42:48 +0000

    Fix references keybinding

--- .config/nvim/lua/stephenpaulger/plugins/lsp.lua
+++ .config/nvim/lua/stephenpaulger/plugins/lsp.lua
@@ -24,7 +24,7 @@ return {
             })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
             vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
         end,
     },
