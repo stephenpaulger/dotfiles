148a93f898eceae2f07e1c7938286131420694e7
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2025-09-22 14:43:47 +0100

    revert to shell cd

--- .config/zsh/zoxide.zsh
+++ .config/zsh/zoxide.zsh
@@ -1,16 +1,1 @@
 eval "$(zoxide init zsh)"
-
-function cd() {
-    if [ "$#" -eq 0 ]; then
-        # No arguments, use builtin cd
-        builtin cd
-    elif builtin cd "$1"; then
-        # cd-ing into the specified directory worked
-        return
-    elif [[ -n "$PS1" && "$+functions[zi]" ]]; then
-        # cd-ing didn't work,
-        # this looks like it's running in a shell and,
-        # the zi function is defined
-        zi "$1"
-    fi
-}
