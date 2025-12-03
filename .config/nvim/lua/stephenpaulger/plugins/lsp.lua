de312d60c39265de9cdc1dead88340516d5bfff0
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2025-12-03 08:26:01 +0000

    rm lspconfig; add typst plugin

--- .config/nvim/lua/stephenpaulger/plugins/lsp.lua
+++ .config/nvim/lua/stephenpaulger/plugins/lsp.lua
@@ -12,32 +12,4 @@ return {
             })
         end,
     },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.gopls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.yamlls.setup({
-                capabilities = capabilities
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
 }
