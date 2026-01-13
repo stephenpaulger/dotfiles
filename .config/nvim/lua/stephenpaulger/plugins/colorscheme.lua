ed973eee05dc8196c0859cd752d6f011593b6d4f
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2026-01-13 10:33:57 +0000

    Revert "Christmas theming"
    This reverts commit 5806b2f26aee52048d71cce1cde68027754c4231.

--- .config/nvim/lua/stephenpaulger/plugins/colorscheme.lua
+++ .config/nvim/lua/stephenpaulger/plugins/colorscheme.lua
@@ -1,17 +1,8 @@
--- return {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000,
---     config = function()
---         vim.cmd.colorscheme("catppuccin-frappe")
---     end,
--- }
-
 return {
-    "chaserensberger/christmas.nvim",
-    name = "christmas",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-        vim.cmd.colorscheme("christmas")
+        vim.cmd.colorscheme("catppuccin-frappe")
     end,
 }
