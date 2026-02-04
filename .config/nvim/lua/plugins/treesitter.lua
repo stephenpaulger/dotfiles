991fe2fd7a6d5a3200e18aaa5d9f42963afc1739
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2026-02-04 22:27:22 +0000

    remove auto install list from treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,20 +4,5 @@ return {
         build = ":TSUpdate",
         branch = "main",
         lazy = false,
-        config = function()
-            local treesitter = require("nvim-treesitter")
-            treesitter.install({
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "javascript",
-                "typescript",
-                "html",
-                "yaml",
-                "python",
-                "typst",
-            })
-        end,
     },
 }
