98b6eafefb7ac4f1b4e7916b5c8bf42543536e89
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2026-09-22 13:49:11 +0100

    Add some git aliases

--- dot-config/zsh/git.zsh
+++ dot-config/zsh/git.zsh
@@ -14,10 +14,17 @@ function git_main_branch() {
   return 1
 }
 
-
 alias gc='git commit --verbose'
 alias gca='git commit --all'
-alias gcm='git checkout $(git_main_branch)'
+
+alias gcm='git switch $(git_main_branch)'
 alias gl="git pull"
-alias gss="git status --short"
 alias gp="git push"
+alias gss="git status --short"
+
+alias gsw='git switch'
+alias gswc='git switch -c'
+alias gswm='git switch $(git_main_branch)'
+
+alias grbm='git rebase $(git_main_branch)'
+alias grbim='git rebase -i $(git_main_branch)'
